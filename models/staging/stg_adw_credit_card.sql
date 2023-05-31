b8f84c5fa41c5c58e4c598b5843a0a5aa8c94f1a
Daniel Arnóbio <daniel.arnobio@indicium.tech>
2023-05-31 17:36:21 +0000

    creating stg_adw_credit_card.sql

---
+++ models/staging/stg_adw_credit_card.sql
@@ -0,0 +1,11 @@
+with credit_card as (
+    select 
+        creditcardid as credit_card_id
+        , cardtype as card_type
+        , cardnumber as card_number
+        , expmonth as exp_month
+        , expyear as exp_year
+    from {{ source('sap_adw', 'creditcard') }}
+)
+select *
+from credit_card
