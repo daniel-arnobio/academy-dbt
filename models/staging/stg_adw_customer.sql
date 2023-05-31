0e7d10746e8d71bc82ac5c96c37acd6b3cd6e563
Daniel Arnóbio <daniel.arnobio@indicium.tech>
2023-05-31 11:05:46 +0000

    deleted storeid from stg_adw_customer.sql

--- models/staging/stg_adw_customer.sql
+++ models/staging/stg_adw_customer.sql
@@ -1,7 +1,6 @@
 with customer as (
     select 
        customerid as customer_id
-       , storeid as stored_id
        , territoryid as territory_id
     from {{ source('sap_adw', 'customer') }}
 )
