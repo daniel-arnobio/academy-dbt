5e0505f6f0df933b6e87f755bf84afb9b145830f
Daniel Arnóbio <daniel.arnobio@indicium.tech>
2023-06-01 01:50:48 +0000

    rename dim_product to dim_products and create sk

--- models/marts/dim_products.sql
+++ models/marts/dim_products.sql
@@ -13,26 +13,30 @@ with
             , name as product_category
         from {{ ref ('stg_adw_product_category') }}
     )
-
     , product_subcategory as (
         select
             product_subcategory_id
             , product_category_id
             , name as product_subcategory
         from {{ ref ('stg_adw_product_subcategory') }}
-
     )
     , dim_product as (
         select
             product.product_id
-            , product.name 
-            , product_category.product_category
-            , product_subcategory.product_subcategory
+            , product.name as product_name
+            , product_category.product_category as product_category_name
+            , product_subcategory.product_subcategory as product_subcategory_name
             , product.safety_stock_level
         from product
         left join product_subcategory on product.product_subcategory_id = product_subcategory.product_category_id
         left join product_category on product_subcategory.product_category_id = product_category.product_category_id
     )
+    , dim_product_sk as (
+            select
+                {{ dbt_utils.generate_surrogate_key(['product_id']) }} as product_sk
+                , *
+           from dim_product
+    )
 select *
-from dim_product
+from dim_product_sk
  
