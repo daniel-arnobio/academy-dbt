5b3f1be738b57dc2caa6a58e9b98b966fb2bfc6f
Daniel Arnóbio <daniel.arnobio@indicium.tech>
2023-06-05 15:32:15 +0000

    update stg_adw_person.sql to bring person_type_description

--- models/staging/stg_adw_person.sql
+++ models/staging/stg_adw_person.sql
@@ -4,6 +4,15 @@ with
             businessentityid as person_id
             , concat(firstname, ' ', middlename, ' ', lastname) as person_name
             , persontype as person_type
+            , case persontype
+                when 'SC' then 'Store Contact'
+                when 'IN' then 'Individual (retail) customer'
+                when 'SP' then 'Sales person'
+                when 'EM' then 'Employee (non-sales)'
+                when 'VC' then 'Vendor contact'
+                when 'GC' then 'General contact'
+                else persontype
+              end as person_type_description
         from {{ source('sap_adw', 'person') }}
     )
 select *
