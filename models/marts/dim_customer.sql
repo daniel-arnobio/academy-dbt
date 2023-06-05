84d16a7e5894fd69f22d215e9a783ad45964f4db
Daniel Arnóbio <daniel.arnobio@indicium.tech>
2023-06-05 18:15:18 +0000

    update dim_customer.sql and add dim_customer.yml

--- models/marts/dim_customer.sql
+++ models/marts/dim_customer.sql
@@ -9,7 +9,9 @@ with
             person_id
             , person_name
             , person_type
+            , person_type_description
         from {{ ref('stg_adw_person') }}
+        where person_type_description = 'Individual (retail) customer'
     ) 
     , store as (
         select
@@ -23,9 +25,18 @@ with
             customer_id
             , person.person_name
             , person.person_type
+            , person.person_type_description
+            , store.sales_person_id
+            , store.store_name
         from customer
         left join person on customer.customer_id = person.person_id
         left join store on person.person_id = store.sales_person_id
     )
+    , dim_customer_sk as (
+        select
+            {{ dbt_utils.generate_surrogate_key(['customer_id']) }} as product_sk
+            , *
+        from dim_customer
+    )
 select *
-from dim_customer
+from dim_customer_sk
