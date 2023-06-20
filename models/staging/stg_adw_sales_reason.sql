e1dd8f8cd90064f4a206eeee78c77190ac8c742a
Daniel Arnóbio <daniel.arnobio@indicium.tech>
2023-06-20 17:42:05 +0000

    correct columns name in stg_adw_sales_reason.sql

--- models/staging/stg_adw_sales_reason.sql
+++ models/staging/stg_adw_sales_reason.sql
@@ -1,9 +1,9 @@
 with 
     sales_reason as (
         select 
-            salesreasonid
+            salesreasonid as sales_reason_id
             , name
-            , reasontype
+            , reasontype as reason_type
         from {{ source('sap_adw', 'salesreason') }}
     )
 select *
