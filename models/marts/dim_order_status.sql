13d22f9ce9306044276235577e44d49d217f7b20
Daniel Arnóbio <daniel.arnobio@indicium.tech>
2023-06-22 15:45:35 +0000

    correct dim_order_status.sql

--- models/marts/dim_order_status.sql
+++ models/marts/dim_order_status.sql
@@ -1,16 +1,31 @@
 with 
-    dim_order_status as (
-        select
+    stg_order_header_sales_reason as (
+        select 
+            sales_order_id
+            , sales_reason_id
+        from {{ ref('stg_adw_sales_order_header_sales_reason') }}
+    )
+    , stg_adw_sales_reason as (
+        select distinct
             sales_reason_id 
             , name
             , reason_type
         from {{ ref('stg_adw_sales_reason') }}
     )
-    , dim_order_status_sk as (
+    , dim_order_status_cte as (
         select
-            {{ dbt_utils.generate_surrogate_key(['sales_reason_id']) }} as sales_reason_sk
+            stg_order_header_sales_reason.sales_order_id
+            , stg_adw_sales_reason.name
+            , stg_adw_sales_reason.reason_type
+        from stg_order_header_sales_reason
+        left join stg_adw_sales_reason
+            on stg_order_header_sales_reason.sales_reason_id =stg_adw_sales_reason.sales_reason_id
+    )
+   , dim_order_status_sk as (
+        select
+            {{ dbt_utils.generate_surrogate_key(['sales_order_id']) }} as sales_reason_sk
             , *
-        from dim_order_status
+        from dim_order_status_cte
     )
 select *
 from dim_order_status_sk
